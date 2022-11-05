a007c3ebf3ebbb19df6d196b5e0bc2ec5af17753
ParshadDoshi <Parshad.doshi@gmail.com>
2022-11-05 15:05:32 +0530

    First commit

---
+++ config/dbconfig.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
+
+dotenv.config();
+module.exports.MongoConnect = async function () {
+    try {
+        await mongoose.connect(process.env.MONGO_URL + '/' + process.env.DB_Name);
+        console.log('Connection is done');
+    } catch (error) {
+        console.log('Error is', error)
+        // handleError(error);
+    }
+}
